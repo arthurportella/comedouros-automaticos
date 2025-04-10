d5cee8aa5d194fa60b8a55d1efc9cffc024e62f9
arthurportella <arthur.portella@ecomp.ufsm.br>
2025-04-10 15:39:01 -0300

    Ajuste de potencia/distancia da antena FM-503

--- Fonkan_FM-503/leitor_fonkan.py
+++ Fonkan_FM-503/leitor_fonkan.py
@@ -14,19 +14,26 @@ try:
     ser.reset_output_buffer()
 
     leitor = Reader(ser)
+
+    # Envia o comando de potência máxima
+    leitor.ser.write(b'\nN1,03\r') # Ajuste de potencia para a antena, em hexadecimal mudar o valor apos a virgula entre as barras
+    time.sleep(0.3)
+    leitor.ser.reset_input_buffer()  # 💡 Limpa a resposta do comando anterior
+
     print("📡 Leitor iniciado. Aguardando tags...\n")
 
     while True:
         tags = leitor.multi_tag_EPC_read()
         if tags:
             for tag in tags:
-                raw_data = tag[0]  # lista de inteiros (dados brutos da tag)
-                tag_id = ''.join(f'{word:04X}' for word in raw_data)  # Formata como HEX contínuo
+                raw_data = tag[0]
+                tag_id = ''.join(f'{word:04X}' for word in raw_data)
                 print(f"🆔 Tag ID: {tag_id}")
 
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     print("\n⛔ Encerrado pelo usuário.")
     ser.close()
 except Exception as e:
-    print(f"❌ Erro: {e}")
+    print(f"❌ Erro geral: {e}")
